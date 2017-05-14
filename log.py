ec4f872269fd1699db64f292c3613ab096463001
Vijay Kumar Gadivenuka <vijay.gadivenuka@st.ovgu.de>
2017-05-14 15:32:47 +0200

    Typo changes

--- log.py
+++ log.py
@@ -1,32 +1,36 @@
-
 import psycopg2
 
+""" Importing the psycopg2 library for Postgresql Database """
+
 def articles():
+    """ articles method is used to print popular articles"""
     pg = psycopg2.connect("dbname=news")
     c = pg.cursor()
     c.execute("select articles.title, count(newpath) as views from articles\
     join new_log on articles.slug=new_log.newpath group by articles.title order\
     by views desc limit 3;")
     article=c.fetchall()
-    print article
+    print ("Popular articles of all time are: \n", article)
     pg.close()
 
 def authors():
+    """ authors method is used to print popular Authors"""
     pg = psycopg2.connect("dbname=news")
     c = pg.cursor()
     c.execute("select authors.name, count(art_log.newpath) as views from\
     authors join art_log on authors.id=art_log.author group by authors.name order by\
     views desc;")
     author=c.fetchall()
-    print author
+    print ("Popular authors of all time are: \n", author)
     pg.close()
 
 def errors():
+    """ errors method is used to print errors during the day"""
     pg = psycopg2.connect("dbname=news")
     c = pg.cursor()
     c.execute("select day, error_ratio from error_rate limit 1;")
     error=c.fetchall()
-    print error
+    print ("Highest error rate on: \n", error)
     pg.close()
 
 articles()
